b873fb748607889f3a6ab48667864a2c5a26e0d4
Richard Moss <richard.moss@cyotek.com>
2020-11-28 10:56:58 +0000

    Converted WinForms demo to .NET Framework
    .NET Core WinForms designer is unusable

--- demo/Program.cs
+++ demo/Program.cs
@@ -24,7 +24,6 @@ namespace Markdig.Keyboard.Demo
     [STAThread]
     private static void Main()
     {
-      Application.SetHighDpiMode(HighDpiMode.SystemAware);
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new MainForm());
---
+++ demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Markdig.Keyboard
+// https://github.com/cyotek/Markdig.Keyboard
+
+// Copyright © 2020 Cyotek Ltd. All Rights Reserved.
+
+// This work is licensed under the MIT License.
+// See LICENSE.TXT for the full text
+
+// Found this example useful?
+// https://www.paypal.me/cyotek
+
+[assembly: AssemblyTitle("Markdig.Keyboard Demonstration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cyotek Ltd")]
+[assembly: AssemblyProduct("Markdig.Keyboard")]
+[assembly: AssemblyCopyright("Copyright © 2020 Cyotek Ltd. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("32d300cd-ccad-4c5f-9b96-fd0130f7ae9c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
